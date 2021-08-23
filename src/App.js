636b9f7e32310b0452a90bf5b1eb50cbd56f8767
Raul Torres <jr.torsa@gmail.com>
2021-08-23 15:48:40 -0600

    making and styling card component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Card from './components/Card';
 function App() {
   return (
     <div>
+      <h1 style={{ textAlign: 'center' }}>Cronomentros</h1>
+      <hr />
       <Card />
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,13 +5,17 @@ const Card = () => {
   return (
     <div className="card">
       <div className="card-container">
-        <span>Crear Nuevos Proyectos</span>
-        <span>Make it Real</span>
-        <span>00:21:17</span>
+        <div className="title">Crear Nuevos Proyectos</div>
+        <div className="sub-title">Make it Real</div>
+        <div className="timer">00:21:17</div>
         <div className="buttons">
-          <button>Gargabe</button>
-          <button>Edit</button>
-          <button>Start</button>
+          <button className="trash">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+          <button className="edit">
+            <i class="far fa-edit"></i>
+          </button>
+          <button className="start-stop">Start</button>
         </div>
       </div>
     </div>
