4ea38ab7bce1a96a2eee7447a2ca38ce4259ad74
Raul Torres <jr.torsa@gmail.com>
2021-08-23 22:11:18 -0600

    creating create component and styled it

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './Create.scss';
 
 const Create = () => {
-  return <div>from create</div>;
+  return (
+    <div className="create">
+      <div className="create-container">
+        <label htmlFor="title">Title</label>
+        <br />
+        <input type="text" />
+        <br />
+        <label htmlFor="project">Project</label>
+        <br />
+        <input type="text" />
+        <div className="create-buttons">
+          <button className="create-btn">Create</button>
+          <button className="create-cancel-btn">Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Create;
